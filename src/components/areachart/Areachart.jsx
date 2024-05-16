a6cd50e5ab93bdbcaec0d42d13665e0f65f61c10
faridmsta <faridmustafyev@gmail.com>
2024-05-16 22:37:39 +0400

    Update

--- src/components/areachart/Areachart.jsx
+++ src/components/areachart/Areachart.jsx
@@ -3,50 +3,81 @@ import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContai
 import './Areachart.css'
 const data = [
   {
-    name: '',
-    uv: 4000,
+    name: 'January',
+    uv: 2342,
     pv: 2400,
     amt: 2400,
   },
   {
-    name: 'Page B',
-    uv: 3000,
+    name: 'February',
+    uv: 242,
     pv: 1398,
     amt: 2210,
   },
   {
-    name: 'Page C',
-    uv: 2000,
+    name: 'March',
+    uv: 582,
     pv: 9800,
     amt: 2290,
   },
   {
-    name: 'Page D',
-    uv: 2780,
+    name: 'April',
+    uv: 1780,
     pv: 3908,
     amt: 2000,
   },
   {
-    name: 'Page E',
+    name: 'May',
     uv: 1890,
     pv: 4800,
     amt: 2181,
   },
   {
-    name: 'Page F',
+    name: 'June',
     uv: 2390,
     pv: 3800,
     amt: 2500,
   },
   {
-    name: 'Page G',
+    name: 'July',
     uv: 3490,
     pv: 4300,
     amt: 2100,
   },
+  {
+    name: 'August',
+    uv: 1900,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: 'September',
+    uv: 1000,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: 'October',
+    uv: 2732,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: 'November',
+    uv: 1430,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: 'December',
+    uv: 3300,
+    pv: 4300,
+    amt: 2100,
+  },
+
 ];
 
-export default class Areachart extends PureComponent {
+export default class Example extends PureComponent {
   static demoUrl = 'https://codesandbox.io/p/sandbox/simple-area-chart-4y9cnl';
 
   render() {
